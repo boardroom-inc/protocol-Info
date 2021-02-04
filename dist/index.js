118242286b77bf553c85a8b9ddc7159f237a2a69
yj <yjkimjunior@gmail.com>
2021-02-04 14:37:33 -0600

    fix: rename and dont ignore dist

---
+++ dist/index.js
@@ -0,0 +1,117 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.protocolBranding = exports.protocols = void 0;
+// supported protocols
+exports.protocols = {
+    compound: {
+        name: "Compound",
+        path: "compound",
+        description: "Compound is a decentralized money market protocol to borrow and lend assets. This Ethereum protocol establishes markets with algorithmically set interest rates.",
+        suffix: "COMP",
+        folder: "compound",
+    },
+    uniswap: {
+        name: "Uniswap",
+        path: "uniswap",
+        description: "Uniswap is a decentralized protocol for automated liquidity provision on Ethereum. The introduction of UNI (ERC-20) serves to foster community-led growth, development, and self-sustainability, enabling shared community ownership and a vibrant, diverse, and dedicated governance system, which will actively guide the protocol towards the future.",
+        suffix: "UNI",
+        folder: "uniswap",
+    },
+    synthetix: {
+        name: "Synthetix",
+        path: "synthetix",
+        description: "Synthetix is a decentralised synthetic asset issuance protocol built on Ethereum. These synthetic assets are collateralized by the Synthetix Network Token (SNX) which when locked in the contract enables the issuance of synthetic assets (Synths). Spartan Council members, who vote on SCCP's, are voted in by staked SNX holders via a continuous nomination proposal.",
+        suffix: "SNX",
+        folder: "synthetix",
+    },
+    yearn: {
+        name: "Yearn",
+        path: "yearn",
+        description: "yEarn is an automated liquidity aggregator offering a number of yield farming opportunities. In order for a proposal to pass or fail, more than 20% of the total staked YFI tokens (Quorum) have to vote. The Majority threshold currently stands at 50%. \n Anyone can stake their YFI in to vote on proposals - once you vote, your YFI will lock for 3 days.",
+        suffix: "YFI",
+        folder: "yearn",
+    },
+    powerpool: {
+        name: "Powerpool",
+        path: "powerpool",
+        description: "PowerPool is a cross-chain lending protocol for governance tokens, including COMP, BAL, LEND, YFI, BZRX, AKRO, and others. It enables the pooling of the tokens by allowing any holder to supply liquidity into a contract and receive an interest rate if there is enough demand. This, in turn, enables any market participant to borrow governance tokens by placing allowed digital assets as collateral.",
+        suffix: "CVP",
+        folder: "compound",
+    },
+    rarible: {
+        name: "Rarible",
+        path: "rarible",
+        description: "Rarible is the premiere NFT marketplace where you can mint, buy and sell digital collectibles without any coding skills. As we are growing in numbers and expanding our presence on the market, Rarible is taking a shift towards a becoming a fully Decentralized Autonomous Organization.",
+        suffix: "RARI",
+        folder: "rarible",
+    },
+    pooltogether: {
+        name: "PoolTogether",
+        path: "pooltogether",
+        description: "PoolTogether is a protocol for no-loss prize games on Ethereum. The protocol enables developers to build their own no-loss prize games and also offers governance-managed no-loss prize games. Prize games are pools of funds whose accrued interest is distributed as prizes.",
+        suffix: "PcDAI",
+        folder: "pooltogether",
+    },
+    gnosis: {
+        name: "Gnosis",
+        path: "gnosis",
+        description: "Gnosis builds new market mechanisms for decentralized finance. Our three interoperable product lines allow you to securely create, trade, and hold digital assets on Ethereum.",
+        suffix: "GNO",
+        folder: "gnosis",
+    },
+    defidollar: {
+        name: "DefiDollar DAO",
+        path: "defidollar",
+        description: "DeFi Dollar is a stablecoin that uses the primitives of DeFi to stay close to the Dollar, protecting users from counterparty risk, holdings seizure risk, bank run risk, etc. DUSD offers investors an opportunity to index varying stablecoins in its single token. This invariably protects such users from any underlying risks that may be attached with such tokens.",
+        suffix: "DFD",
+        folder: "defidollar",
+    },
+    indexCoop: {
+        name: "Index",
+        path: "index",
+        description: "Index Coop creates and maintains the world's best crypto index products. Index Coop is a Decentralized and Autonomous Asset Manager governed, maintained, and upgraded by INDEX token holders. All index products are always fully collateralized.",
+        suffix: "$INDEX",
+        folder: "indexCoop",
+    },
+    siren: {
+        name: "Siren",
+        path: "siren",
+        description: "Siren is a distributed protocol for creating, trading, and redeeming fully-collateralized options contracts for any ERC-20 token on Ethereum.",
+        suffix: "SI",
+        folder: "siren",
+    },
+    aave: {
+        name: "Aave",
+        path: "aave",
+        description: "Aave is an open source and non-custodial protocol enabling the creation of money markets. Users can earn interest on deposits and also borrow assets.",
+        suffix: "LEND",
+        folder: "aave",
+    },
+    balancer: {
+        name: "Balancer",
+        path: "balancer",
+        description: "(balancer description here)",
+        suffix: "BAL",
+        folder: "balancer",
+    },
+    yamv2: {
+        name: "Yam",
+        path: "yamv2",
+        description: "(yamv2 description here)",
+        suffix: "YAM",
+        folder: "yamv2",
+    },
+};
+exports.protocolBranding = {
+    pooltogether: {
+        "--background-primary-nav": "#290b5a",
+        "--color-text-nav": "#e7e8eb",
+        "--background-switcher-active": "#341762",
+    },
+    defidollar: {
+        "--background-primary-nav": "#212529",
+        "--color-text-nav": "#e7e8eb",
+        "--background-switcher-active": "#5553e8",
+    },
+};
--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 export default {
   input: './index.ts',
   output: {
-    dir: 'build', format: 'cjs'
+    dir: 'dist', format: 'cjs'
   },
   plugins: [typescript()]
 };
