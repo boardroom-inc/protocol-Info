a436a0adbd9a3526512ce9c0827312c5659670a8
sudheer <sudheer.105@gmail.com>
2021-03-03 23:06:18 +0530

    Update synthetix path

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ export const protocols = {
   },
   synthetix: {
     name: "Synthetix",
-    path: "synthetixproposal",
+    path: "snxgov.eth",
     description:
       "Synthetix is a decentralised synthetic asset issuance protocol built on Ethereum.",
     suffix: "SNX",
