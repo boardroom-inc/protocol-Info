8554371dd810327c9074ef48470059fe340bd9ca
Zak Angelle <zaknotzak@gmail.com>
2021-04-02 00:43:33 -0500

    concatenate error messages

--- scripts/validate.ts
+++ scripts/validate.ts
@@ -2,6 +2,29 @@ import { validator } from "io-ts-validator";
 import { ProtocolIo } from "../types";
 import protocols from "../dist";
 
+const errors = [];
+
 for (const protocol of protocols) {
-  validator(ProtocolIo).decodeSync(protocol);
+  try {
+    validator(ProtocolIo).decodeSync(protocol);
+  } catch (e) {
+    errors.push({
+      protocol: protocol.name,
+      message: e,
+    });
+  }
+}
+
+const errorMessage = errors.reduce(
+  (message, error) =>
+    `
+    ${message}\n
+    Error validating protocol: ${error.protocol}\n
+    ${error.message}
+  `,
+  ""
+);
+
+if (errorMessage) {
+  throw new Error(errorMessage);
 }
