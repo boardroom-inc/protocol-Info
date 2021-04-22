f17780856ffcd05e073b3d73140cb3e19e1eb479
yj <yjkimjunior@gmail.com>
2021-04-22 12:46:44 -0500

    fix: add new protocl script

--- scripts/add_new_protocol.sh
+++ scripts/add_new_protocol.sh
@@ -8,7 +8,16 @@ do
         echo "Pass in the name of the protocol whose info you want to add. e.g: ./add_new_protocol.sh rarible"
 		else
 			mkdir -p ./$arg
-			mkdir -p ./$arg/calls
+			
+			mkdir -p ./$arg/resources
+			mkdir -p ./$arg/resources/calls
+			mkdir -p ./$arg/resources/Gov Weekly
+			
+			mkdir -p ./$arg/contracts
+			touch ./$arg/contracts/governance.json
+			touch ./$arg/contracts/token.json
+
+			touch ./$arg/index.json
 			touch ./$arg/events.json
 			touch ./$arg/overview.md
 			touch ./$arg/logo.png
